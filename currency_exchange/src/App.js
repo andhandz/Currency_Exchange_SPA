8feb3b66ba550ebdf904faaacce645e70e21e569
andhandz <andhandz0125@gmail.com>
2023-02-23 19:25:18 +0100

    add version using api

--- currency_exchange/src/App.js
+++ currency_exchange/src/App.js
@@ -3,12 +3,26 @@ import './App.css';
 
 function App() {
   const [GBPValue, setGBPValue] = useState(0);
+  const [rate, setRate] = useState(0);
+  const url = "http://api.nbp.pl/api/exchangerates/rates/a/gbp/?format=json";
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setRate(data.rates[0].mid);
+    }
+    catch (error){
+      console.log(error);
+    }
+  };
 
   const handleGBPChange = (event) => {
     setGBPValue(event.target.value);
   };
 
-  const zlotyValue=GBPValue*5
+  fetchData();
+  const zlotyValue=(GBPValue*rate.toFixed(2)).toFixed(2);
 
   return (
     <><div className="form-group">
@@ -31,7 +45,7 @@ function App() {
         </span>
       </label>
     </div><div className="info">
-        <text>1 GBP = 5 PLN</text>
+        <text>1 GBP = {rate.toFixed(2)} PLN</text>
         <br />
         <h3>No transfer fee</h3>
       </div></>
